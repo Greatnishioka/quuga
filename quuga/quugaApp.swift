497eec0e0f9bf9d3cc5eb1d67abcfdabfe222cfe
Greatenishioka <20e1118@andrew.ac.jp>
2026-01-08 01:03:08 +0900

    Initial Commit

---
+++ quuga/quugaApp.swift
@@ -0,0 +1,17 @@
+//
+//  quugaApp.swift
+//  quuga
+//
+//  Created by 西岡 on 2026/01/08.
+//
+
+import SwiftUI
+
+@main
+struct quugaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ quugaTests/quugaTests.swift
@@ -0,0 +1,17 @@
+//
+//  quugaTests.swift
+//  quugaTests
+//
+//  Created by 西岡 on 2026/01/08.
+//
+
+import Testing
+@testable import quuga
+
+struct quugaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
